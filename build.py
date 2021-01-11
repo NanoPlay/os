cdb41b0ce6b01d94ca37387f0f186b37add5aaa9
James-Livesey <jamesliveg@gmail.com>
2021-01-11 12:15:24 +0000

    Add correct comment padding for copyright notice in build script

--- build.py
+++ build.py
@@ -1,5 +1,7 @@
 # NanoPlay
+# 
 # Copyright (C) Subnodal Technologies. All Rights Reserved.
+# 
 # https://nanoplay.subnodal.com
 # Licenced by the Subnodal Open-Source Licence, which can be found at LICENCE.md.
 
