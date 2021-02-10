2187e6b5ca2d1201adf6b822ff898acbb713498b
James-Livesey <jamesliveg@gmail.com>
2021-02-10 17:27:58 +0000

    Integrate subDoc for API reference

--- api.js
+++ api.js
@@ -9,9 +9,25 @@
 
 exports._communicators = "";
 
+/*
+    @name OS_VERSION
+    @type const <String>
+    The version string of NanoPlay OS.
+*/
 exports.OS_VERSION = require("config").OS_VERSION;
+
+/*
+    @name OS_VERNUM
+    @type const <Number>
+    The comparable version number of NanoPlay OS.
+*/
 exports.OS_VERNUM = require("config").OS_VERNUM;
 
+/*
+    @name fillShapes
+    @type var <Boolean>
+    Whether to fill in the insides of shapes.
+*/
 exports.fillShapes = false;
 
 function typeAll(params, type) {
@@ -32,15 +48,25 @@ function typePin(pin) {
     }
 }
 
+/*
+    @name close
+    Close the app and return to the home screen.
+*/
 exports._communicators += ";var close=" + (function() {
     _shouldClose = true;
 }).toString();
 
+/*
+    @name statusBar
+    Enable/disable showing the status bar. The status bar is drawn over everything else.
+    @param enable <Boolean = false> Whether to show the status bar
+*/
 exports._communicators += ";var statusBar=" + (function(enable) {
     _showStatusBar = !!enable;
 }).toString();
 
 /*
+    @name clear
     Clear the screen.
 */
 exports.clear = function() {
@@ -48,21 +74,20 @@ exports.clear = function() {
 };
 
 /*
-    Enable/disable the filling in of subsequent shapes. If disabled, only shape
-    outlines are drawn.
-
-    @param enable {Boolean = false} Whether to fill in subsequent shapes
+    @name fill
+    Enable/disable the filling in of subsequent shapes. If disabled, only shape outlines are drawn.
+    @param enable <Boolean = false> Whether to fill in subsequent shapes
 */
 exports.fill = function(enable) {
     exports.fillShapes = !!enable;
 };
 
 /*
+    @name invert
     Enable/disable the inversion of subequent graphics. If enabled, filled-in
     shapes will appear as off whereas they would appear as on when inversion is
     disabled.
-
-    @param enable {Boolean = false} Whether to invert subsequent graphics
+    @param enable <Boolean = false> Whether to invert subsequent graphics
 */
 exports.invert = function(enable) {
     g.setBgColor(enable & 1);
@@ -70,12 +95,12 @@ exports.invert = function(enable) {
 };
 
 /*
+    @name line
     Draw a line from one coordinate to another.
-
-    @param x1 {Number} The X component of the first coordinate
-    @param y1 {Number} The Y component of the first coordinate
-    @param x2 {Number} The X component of the second coordinate
-    @param y2 {Number} The Y component of the second coordinate
+    @param x1 <Number> The X component of the first coordinate
+    @param y1 <Number> The Y component of the first coordinate
+    @param x2 <Number> The X component of the second coordinate
+    @param y2 <Number> The Y component of the second coordinate
 */
 exports.line = function(x1, y1, x2, y2) {
     typeAll(arguments, "number");
@@ -84,12 +109,12 @@ exports.line = function(x1, y1, x2, y2) {
 };
 
 /*
+    @name rect
     Draw a rectangle from a given coordinate.
-
-    @param x {Number} The X component of the origin coordinate
-    @param y {Number} The Y component of the origin coordinate
-    @param w {Number} The width of the rectangle
-    @param h {Number} The height of the rectangle
+    @param x <Number> The X component of the origin coordinate
+    @param y <Number> The Y component of the origin coordinate
+    @param w <Number> The width of the rectangle
+    @param h <Number> The height of the rectangle
 */
 exports.rect = function(x, y, w, h) {
     typeAll(arguments, "number");
@@ -102,11 +127,11 @@ exports.rect = function(x, y, w, h) {
 };
 
 /*
+    @name circle
     Draw a circle around a given coordinate with a specified radius.
-
-    @param x {Number} The X component of the origin coordinate
-    @param y {Number} The Y component of the origin coordinate
-    @param radius {Number} The radius of the circle
+    @param x <Number> The X component of the origin coordinate
+    @param y <Number> The Y component of the origin coordinate
+    @param radius <Number> The radius of the circle
 */
 exports.circle = function(x, y, radius) {
     typeAll(arguments, "number");
@@ -119,12 +144,12 @@ exports.circle = function(x, y, radius) {
 };
 
 /*
+    @name ellipse
     Draw an ellipse from a given coordinate.
-
-    @param x {Number} The X component of the origin coordinate
-    @param y {Number} The Y component of the origin coordinate
-    @param w {Number} The width of the ellipse
-    @param h {Number} The height of the ellipse
+    @param x <Number> The X component of the origin coordinate
+    @param y <Number> The Y component of the origin coordinate
+    @param w <Number> The width of the ellipse
+    @param h <Number> The height of the ellipse
 */
 exports.ellipse = function(x, y, w, h) {
     typeAll(arguments, "number");
@@ -137,12 +162,12 @@ exports.ellipse = function(x, y, w, h) {
 };
 
 /*
+    @name text
     Draw a string of text from a given coordinate.
-
-    @param x {Number} The X component of the origin coordinate
-    @param y {Number} The Y component of the origin coordinate
-    @param text {*} The string to be drawn as text
-    @param mini {Boolean = false} Whether to draw the text in a smaller font
+    @param x <Number> The X component of the origin coordinate
+    @param y <Number> The Y component of the origin coordinate
+    @param text <*> The string to be drawn as text
+    @param mini <Boolean = false> Whether to draw the text in a smaller font
 */
 exports.text = function(x, y, text) {
     var mini = arguments[3] || false;
@@ -159,12 +184,11 @@ exports.text = function(x, y, text) {
 };
 
 /*
+    @name getTextWidth
     Calculate the width of a given string of text
-
-    @param text {*} The string to calculate the textual width of
-    @param mini {Boolean = false} Whether the text is set in a smaller font
-    
-    @returns {Number} The width of the text in pixels
+    @param text <*> The string to calculate the textual width of
+    @param mini <Boolean = false> Whether the text is set in a smaller font
+    @returns <Number> The width of the text in pixels
 */
 exports.getTextWidth = function(text) {
     var mini = arguments[1] || false;
@@ -179,12 +203,11 @@ exports.getTextWidth = function(text) {
 };
 
 /*
+    @name getPixel
     Get a specified pixel's value.
-
-    @param x {Number} The X component of the pixel's coordinate
-    @param y {Number} The Y component of the pixel's coordinate
-
-    @returns {Boolean} Whether the pixel is on or off
+    @param x <Number> The X component of the pixel's coordinate
+    @param y <Number> The Y component of the pixel's coordinate
+    @returns <Boolean> Whether the pixel is on or off
 */
 exports.getPixel = function(x, y) {
     typeAll(arguments, "number");
@@ -193,11 +216,11 @@ exports.getPixel = function(x, y) {
 };
 
 /*
+    @name setPixel
     Set a specified pixel's value.
-
-    @param x {Number} The X component of the pixel's coordinate
-    @param y {Number} The Y component of the pixel's coordinate
-    @param on {Boolean = true} Whether the pixel is on or off
+    @param x <Number> The X component of the pixel's coordinate
+    @param y <Number> The Y component of the pixel's coordinate
+    @param on <Boolean = true> Whether the pixel is on or off
 */
 exports.setPixel = function(x, y) {
     var on = arguments[2] || true;
@@ -209,9 +232,9 @@ exports.setPixel = function(x, y) {
 
 exports.tl = {
     /*
+        @name tl.pressed
         Whether the top-left button is pressed.
-
-        @returns {Boolean} Whether the button is pressed
+        @returns <Boolean> Whether the button is pressed
     */
     pressed: function() {
         return BTN1.read();
@@ -220,9 +243,9 @@ exports.tl = {
 
 exports.tr = {
     /*
+        @name tr.pressed
         Whether the top-right button is pressed.
-
-        @returns {Boolean} Whether the button is pressed
+        @returns <Boolean> Whether the button is pressed
     */
     pressed: function() {
         return BTN2.read();
@@ -231,9 +254,9 @@ exports.tr = {
 
 exports.bl = {
     /*
+        @name bl.pressed
         Whether the bottom-left button is pressed.
-
-        @returns {Boolean} Whether the button is pressed
+        @returns <Boolean> Whether the button is pressed
     */
     pressed: function() {
         return BTN4.read();
@@ -242,9 +265,9 @@ exports.bl = {
 
 exports.br = {
     /*
+        @name br.pressed
         Whether the bottom-right button is pressed.
-
-        @returns {Boolean} Whether the button is pressed
+        @returns <Boolean> Whether the button is pressed
     */
     pressed: function() {
         return BTN3.read();
@@ -252,11 +275,10 @@ exports.br = {
 };
 
 /*
+    @name readPin
     Read an analog pin's value (on the back of the NanoPlay).
-
-    @param pin {Number} Index of the pin: an integer within the bounds 0 to 5 inclusive
-
-    @returns {Number} The analog value of the pin: a real number within the bounds 0 to 1 inclusive
+    @param pin <Number> Index of the pin: an integer within the bounds 0 to 5 inclusive
+    @returns <Number> The analog value of the pin: a real number within the bounds 0 to 1 inclusive
 */
 exports.readPin = function(pin) {
     typePin(pin);
@@ -265,10 +287,10 @@ exports.readPin = function(pin) {
 };
 
 /*
+    @name writePin
     Write an analog pin's value (on the back of the NanoPlay).
-
-    @param pin {Number} Index of the pin: an integer within the bounds 0 to 5 inclusive
-    @param value {Number} The The analog value of the pin: a real number within the bounds 0 to 1 inclusive
+    @param pin <Number> Index of the pin: an integer within the bounds 0 to 5 inclusive
+    @param value <Number> The The analog value of the pin: a real number within the bounds 0 to 1 inclusive
 */
 exports.writePin = function(pin, value) {
     typePin(pin);
@@ -283,11 +305,10 @@ exports.writePin = function(pin, value) {
 };
 
 /*
+    @name readFile
     Read a data file from the NanoPlay's storage.
-
-    @param filename {String} The filename to read from: a string with only characters a-z, A-Z and 0-9, between 1 to 20 characters long
-
-    @returns {String} The contents of the data file that have been read
+    @param filename <String> The filename to read from: a string with only characters a-z, A-Z and 0-9, between 1 to 20 characters long
+    @returns <String> The contents of the data file that have been read
 */
 exports.readFile = function(filename) {
     typeAll([filename], "string");
@@ -304,11 +325,11 @@ exports.readFile = function(filename) {
 };
 
 /*
+    @name writeFile
     Write a data file to the NanoPlay's storage. Any data files with the same
     name will be overwritten.
-
-    @param filename {String} The filename to write to: a string with only characters a-z, A-Z and 0-9, between 1 to 20 characters long
-    @param data {String} The contents of the data file to write
+    @param filename <String> The filename to write to: a string with only characters a-z, A-Z and 0-9, between 1 to 20 characters long
+    @param data <String> The contents of the data file to write
 */
 exports.writeFile = function(filename, data) {
     typeAll([filename], "string");
@@ -329,9 +350,9 @@ exports.writeFile = function(filename, data) {
 };
 
 /*
+    @name getFileList
     Get a list of data files in the NanoPlay's storage.
-
-    @returns {Object} A list of filenames of data files
+    @returns <Object> A list of filenames of data files
 */
 exports.getFileList = function() {
     return require("Storage").list()
@@ -341,9 +362,9 @@ exports.getFileList = function() {
 };
 
 /*
+    @name nfcSet
     Set the NanoPlay's NFC chip to emit the specified URL to nearby devices.
-
-    @param url {String} The URL to emit to nearby devices
+    @param url <String> The URL to emit to nearby devices
 */
 exports.nfcSet = function(url) {
     typeAll([url], "string");
@@ -352,46 +373,45 @@ exports.nfcSet = function(url) {
 };
 
 /*
+    @name getBatteryPercentage
     Get the battery's fullness percentage.
-
-    @returns {Number} The percentage fullness of the battery: a number within the bounds of 0 to 100 inclusive
+    @returns <Number> The percentage fullness of the battery: a number within the bounds of 0 to 100 inclusive
 */
 exports.getBatteryPercentage = function() {
     return E.getBattery();
 };
 
 /*
+    @name getTemperatureCelsius
     Get the current measured temperature in degrees Celsius.
-
-    @returns {Number} The temperature in degrees Celsius
+    @returns <Number> The temperature in degrees Celsius
 */
 exports.getTemperatureCelsius = function() {
     return E.getTemperature();
 };
 
 /*
+    @name getTemperatireFahrenheit
     Get the current measured temperature in degrees Fahrenheit.
-
-    @returns {Number} The temperature in degrees Fahrenheit
+    @returns <Number> The temperature in degrees Fahrenheit
 */
 exports.getTemperatureFahrenheit = function() {
     return (exports.getTemperatureCelsius() * 1.8) + 32;
 };
 
 /*
+    @name getTemperatureKelvin
     Get the current measured temperature in Kelvin.
-
-    @returns {Number} The temperature in Kelvin
+    @returns <Number> The temperature in Kelvin
 */
 exports.getTemperatureKelvin = function() {
     return exports.getTemperatureCelsius() + 273.15;
 };
 
 /*
-    Get the NanoPlay's current locale code. For example, for English (United
-    Kingdom), the locale code would be `"en_GB"`.
-
-    @returns {String} The locale code of the currently-set locale
+    @name getLocaleCode
+    Get the NanoPlay's current locale code. For example, for English (United Kingdom), the locale code would be `"en_GB"`.
+    @returns <String> The locale code of the currently-set locale
 */
 exports.getLocaleCode = function() {
     return require("l10n").getLocaleCode();
---
+++ docs/global.md
@@ -0,0 +1,189 @@
+# (global)
+## 🔒️ `OS_VERNUM`
+`const <Number>` · The comparable version number of NanoPlay OS.
+
+## 🔒️ `OS_VERSION`
+`const <String>` · The version string of NanoPlay OS.
+
+## ▶️ `bl.pressed`
+`function` · Whether the bottom-left button is pressed.
+
+**Returns:** `Boolean` · Whether the button is pressed
+
+## ▶️ `br.pressed`
+`function` · Whether the bottom-right button is pressed.
+
+**Returns:** `Boolean` · Whether the button is pressed
+
+## ▶️ `circle`
+`function` · Draw a circle around a given coordinate with a specified radius.
+
+**Parameters:**
+* **`x`** (`Number`): The X component of the origin coordinate
+* **`y`** (`Number`): The Y component of the origin coordinate
+* **`radius`** (`Number`): The radius of the circle
+
+## ▶️ `clear`
+`function` · Clear the screen.
+
+## ▶️ `close`
+`function` · Close the app and return to the home screen.
+
+## ▶️ `ellipse`
+`function` · Draw an ellipse from a given coordinate.
+
+**Parameters:**
+* **`x`** (`Number`): The X component of the origin coordinate
+* **`y`** (`Number`): The Y component of the origin coordinate
+* **`w`** (`Number`): The width of the ellipse
+* **`h`** (`Number`): The height of the ellipse
+
+## ▶️ `fill`
+`function` · Enable/disable the filling in of subsequent shapes. If disabled, only shape outlines are drawn.
+
+**Parameters:**
+* **`enable`** (`Boolean` = `false`): Whether to fill in subsequent shapes
+
+## 🔠️ `fillShapes`
+`var <Boolean>` · Whether to fill in the insides of shapes.
+
+## ▶️ `getBatteryPercentage`
+`function` · Get the battery's fullness percentage.
+
+**Returns:** `Number` · The percentage fullness of the battery: a number within the bounds of 0 to 100 inclusive
+
+## ▶️ `getFileList`
+`function` · Get a list of data files in the NanoPlay's storage.
+
+**Returns:** `Object` · A list of filenames of data files
+
+## ▶️ `getLocaleCode`
+`function` · Get the NanoPlay's current locale code. For example, for English (United Kingdom), the locale code would be `"en_GB"`.
+
+**Returns:** `String` · The locale code of the currently-set locale
+
+## ▶️ `getPixel`
+`function` · Get a specified pixel's value.
+
+**Parameters:**
+* **`x`** (`Number`): The X component of the pixel's coordinate
+* **`y`** (`Number`): The Y component of the pixel's coordinate
+
+**Returns:** `Boolean` · Whether the pixel is on or off
+
+## ▶️ `getTemperatireFahrenheit`
+`function` · Get the current measured temperature in degrees Fahrenheit.
+
+**Returns:** `Number` · The temperature in degrees Fahrenheit
+
+## ▶️ `getTemperatureCelsius`
+`function` · Get the current measured temperature in degrees Celsius.
+
+**Returns:** `Number` · The temperature in degrees Celsius
+
+## ▶️ `getTemperatureKelvin`
+`function` · Get the current measured temperature in Kelvin.
+
+**Returns:** `Number` · The temperature in Kelvin
+
+## ▶️ `getTextWidth`
+`function` · Calculate the width of a given string of text
+
+**Parameters:**
+* **`text`** (`*`): The string to calculate the textual width of
+* **`mini`** (`Boolean` = `false`): Whether the text is set in a smaller font
+
+**Returns:** `Number` · The width of the text in pixels
+
+## ▶️ `invert`
+`function` · Enable/disable the inversion of subequent graphics. If enabled, filled-in shapes will appear as off whereas they would appear as on when inversion is disabled.
+
+**Parameters:**
+* **`enable`** (`Boolean` = `false`): Whether to invert subsequent graphics
+
+## ▶️ `line`
+`function` · Draw a line from one coordinate to another.
+
+**Parameters:**
+* **`x1`** (`Number`): The X component of the first coordinate
+* **`y1`** (`Number`): The Y component of the first coordinate
+* **`x2`** (`Number`): The X component of the second coordinate
+* **`y2`** (`Number`): The Y component of the second coordinate
+
+## ▶️ `nfcSet`
+`function` · Set the NanoPlay's NFC chip to emit the specified URL to nearby devices.
+
+**Parameters:**
+* **`url`** (`String`): The URL to emit to nearby devices
+
+## ▶️ `readFile`
+`function` · Read a data file from the NanoPlay's storage.
+
+**Parameters:**
+* **`filename`** (`String`): The filename to read from: a string with only characters a-z, A-Z and 0-9, between 1 to 20 characters long
+
+**Returns:** `String` · The contents of the data file that have been read
+
+## ▶️ `readPin`
+`function` · Read an analog pin's value (on the back of the NanoPlay).
+
+**Parameters:**
+* **`pin`** (`Number`): Index of the pin: an integer within the bounds 0 to 5 inclusive
+
+**Returns:** `Number` · The analog value of the pin: a real number within the bounds 0 to 1 inclusive
+
+## ▶️ `rect`
+`function` · Draw a rectangle from a given coordinate.
+
+**Parameters:**
+* **`x`** (`Number`): The X component of the origin coordinate
+* **`y`** (`Number`): The Y component of the origin coordinate
+* **`w`** (`Number`): The width of the rectangle
+* **`h`** (`Number`): The height of the rectangle
+
+## ▶️ `setPixel`
+`function` · Set a specified pixel's value.
+
+**Parameters:**
+* **`x`** (`Number`): The X component of the pixel's coordinate
+* **`y`** (`Number`): The Y component of the pixel's coordinate
+* **`on`** (`Boolean` = `true`): Whether the pixel is on or off
+
+## ▶️ `statusBar`
+`function` · Enable/disable showing the status bar. The status bar is drawn over everything else.
+
+**Parameters:**
+* **`enable`** (`Boolean` = `false`): Whether to show the status bar
+
+## ▶️ `text`
+`function` · Draw a string of text from a given coordinate.
+
+**Parameters:**
+* **`x`** (`Number`): The X component of the origin coordinate
+* **`y`** (`Number`): The Y component of the origin coordinate
+* **`text`** (`*`): The string to be drawn as text
+* **`mini`** (`Boolean` = `false`): Whether to draw the text in a smaller font
+
+## ▶️ `tl.pressed`
+`function` · Whether the top-left button is pressed.
+
+**Returns:** `Boolean` · Whether the button is pressed
+
+## ▶️ `tr.pressed`
+`function` · Whether the top-right button is pressed.
+
+**Returns:** `Boolean` · Whether the button is pressed
+
+## ▶️ `writeFile`
+`function` · Write a data file to the NanoPlay's storage. Any data files with the same name will be overwritten.
+
+**Parameters:**
+* **`filename`** (`String`): The filename to write to: a string with only characters a-z, A-Z and 0-9, between 1 to 20 characters long
+* **`data`** (`String`): The contents of the data file to write
+
+## ▶️ `writePin`
+`function` · Write an analog pin's value (on the back of the NanoPlay).
+
+**Parameters:**
+* **`pin`** (`Number`): Index of the pin: an integer within the bounds 0 to 5 inclusive
+* **`value`** (`Number`): The The analog value of the pin: a real number within the bounds 0 to 1 inclusive
